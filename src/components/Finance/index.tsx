17072fdc0ec3b98224a8fdcb4ea6ed8bd0eb5d00
lotarev <foermanDT@gmail.com>
2021-01-24 02:30:55 +0300

    add list

--- src/components/Finance/index.tsx
+++ src/components/Finance/index.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect} from "react";
 import "./Finance.css"
-import {AddItem} from "./components/AddItem";
 import {Route, useHistory} from "react-router-dom";
 import {routes} from "../../routes";
+import {FiMinusCircle} from "react-icons/all";
+import {useDrag} from "react-use-gesture";
+import {animated, useSpring} from "react-spring";
 
 export function Finance() {
 
@@ -11,11 +13,38 @@ export function Finance() {
         history.push(routes.finance.root)
     }, [])
 
+    const [swipe, setSwipe] = useSpring(() => ({transform: 'translateX(0px)'}))
+    const touch = useDrag(({down, movement: [mx]}) => {
+        if (mx <= 0 && down) {
+            setSwipe({transform: `translateX(${mx}px)`});
+        }
+    })
 
     return (
         <div className="finance">
             <Route path={routes.finance.root}>
-                <AddItem/>
+                <ul className="finance-list">
+                    <animated.li style={swipe} {...touch()}>
+                        <span>
+                            Разходы на пасуду
+                        </span>
+                        <span className="spending">
+                            100
+                        <FiMinusCircle/>
+                        </span>
+                    </animated.li>
+                    <li>
+                        <span>
+                            Доход на пасуду
+                        </span>
+                        <span className="income">
+                            100
+                        <FiMinusCircle/>
+                        </span>
+                    </li>
+                </ul>
+
+                {/*<AddItem/>*/}
             </Route>
         </div>
     )
