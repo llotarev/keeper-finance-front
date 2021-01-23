e83171df704574f3081b6743cf97c9c695c34944
lotarev <foermanDT@gmail.com>
2021-01-23 11:39:04 +0300

    fix input

--- src/components/Finance/index.tsx
+++ src/components/Finance/index.tsx
@@ -9,10 +9,13 @@ export function Finance() {
             <form action="#">
                 <Input placeholder="Имя"/>
                 <Input type="tel" placeholder="Сумма"/>
+                <Input disabled type="tel" placeholder="Сумма"/>
                 <Button>Отправить</Button>
+                <Button variant="link">Отправить</Button>
             </form>
             <div className="card">
-
+                <Input variant="dark" type="tel" placeholder="Сумма"/>
+                <Input variant="dark" disabled type="tel" placeholder="Сумма"/>
             </div>
         </div>
     )
