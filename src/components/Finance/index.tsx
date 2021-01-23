142d873463587fce2ea79faadb79de5dd26fcda7
lotarev <foermanDT@gmail.com>
2021-01-23 12:10:07 +0300

    fix

--- src/components/Finance/index.tsx
+++ src/components/Finance/index.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Finance.css"
 import {Input} from "../UIKit/Input";
 import {Button} from "../UIKit/Button";
 
 export function Finance() {
+
+    const [font, setFont] = useState("");
+
+    function handleFocus(e: React.FocusEvent<HTMLInputElement>) {
+        setFont("24")
+    }
+
+    function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+        setFont("18")
+    }
+
     return (
         <div className="finance">
             <form action="#">
                 <Input placeholder="Имя"/>
-                <Input type="tel" placeholder="Сумма"/>
-                <Input disabled type="tel" placeholder="Сумма"/>
+                <Input type="range" min="14" max="64"
+
+                       placeholder="Имя"/>
+                <Input type="tel"
+                       onFocus={handleFocus}
+                       onBlur={handleBlur}
+                       style={{fontSize: font + "px"}} placeholder="Динамический размер"/>
                 <Button>Отправить</Button>
                 <Button variant="link">Отправить</Button>
             </form>
